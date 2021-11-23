884525ba41e5ed7c6e976b3b08cedb46befb9421
KolouchPetr <kolouch.petr4@email.cz>
2021-11-23 10:40:09 +0100

    added structs

--- structs.h
+++ structs.h
@@ -1,3 +1,39 @@
-typedef struct testType {
+typedef struct testType
+{
     int test;
 } TestType;
+
+typedef enum
+{
+    Set,
+    Relation
+} SubjectType;
+
+typedef struct
+{
+    char *x;
+    char *y;
+} Pair;
+
+typedef struct
+{
+    int id;
+    int size;
+    Pair pairs[];
+} Relation;
+
+typedef struct
+{
+    int id;
+    int size;
+    char **obsah;
+} Set;
+
+typedef struct
+{
+    int id;
+    Set *set_p;
+    Relation *relation_p;
+    SubjectType subjectType;
+
+} Subject;
