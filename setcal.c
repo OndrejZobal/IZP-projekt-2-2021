de4badd2ee89198f3030484c489ecc4d8d094944
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2021-11-23 10:32:17 +0100

    Reading arguments and added structs file

--- setcal.c
+++ setcal.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "./structs.h"
+
+
+
+// TODO Az bude struktura hotova, tak to bude vracet array ty struktury
+void parseFile(char* filePath) {
+
+}
+
+char* readFilePath(int argc, char **argv) {
+    if (argc < 1) {
+        return NULL;
+    }
+
+    // We can add more checks here lol.
+    return argv[1];
+}
 
 int main(int argc, char **argv) {
-    printf("Hello Všechny body");
+    // Getting the path to the file.
+    char* filePath = readFilePath(argc, argv);
+    if (filePath == NULL) {
+        return 1;
+    }
+
+    // Parsing the content into structs.
+    parseFile(filePath); // TODO Dunno co to bude vracet to vymyslíme později
     return 0;
 }
---
+++ structs.h
@@ -0,0 +1,3 @@
+typedef struct testType {
+    int test;
+} TestType;
