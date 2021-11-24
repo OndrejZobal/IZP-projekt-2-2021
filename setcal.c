cccb1f152a056b07f9a9aaf5a385836ab551a9a4
KolouchPetr <kolouch.petr4@email.cz>
2021-11-24 19:17:39 +0100

    added parseFile function, that reads from file

--- setcal.c
+++ setcal.c
@@ -11,14 +11,17 @@
 #include "./growstr.c"
 
 // TODO UPDATE: Obsah univerza si budeme pamatovat a seznam položek budme používat jako slovník id, abychom si ušetřili porovnavani stringu.
-
-Subject parseLine(char *line) {
+/*
+Subject parseLine(char *line)
+{
+    Subject subjekt = 
     // Calls a lot of helper fucntions
 
     // The result is encapsulated in Subject.
 
-    return malloc(sizeof(Subject));
+    //return malloc(sizeof(Subject));
 }
+*/
 
 /**
  * Parses the intire file into subjects. Calls many helper functions
@@ -26,21 +29,48 @@ Subject parseLine(char *line) {
  * @param subjc The count of subjects in subjv
  * @param subjv Empty pointer for returning the subjects.
  */
-void parseFile(char* filePath, int *subjc, Subject *subjv) {
+void parseFile(char *filePath, int *subjc, Subject *subjv)
+{
+
     FILE *file = fopen(filePath, "r");
+    // current character returned from getc
+    char character;
+    // string to return growStrComvertToStr to
+    char *string;
+    subjv = malloc(sizeof(Subject) * 1000); // Before we have generic grow type...
+                                            // For every line:
+                                            //  Read line char by char and store it in growstr.
+    GrowStr *gs = growStrCreate();
+    printf("Grow string created!\n");
 
-    subjv = malloc(sizeof(Subject)*1000); // Before we have generic grow type...
-    // For every line:
-    //  Read line char by char and store it in growstr.
-
+    // read current charatcter while not at the end of the file
+    while ((character = getc(file)) != EOF)
+    {
+        // if endline is reached
+        // store the whole string into the string variable
+        // call the parsline function with it
+        // free the old references and create new growStr
+        if (character == '\n')
+        {
+            printf("FINAL: %s\n", gs->content);
+            string = growStrConvertToStr(gs);
+            parseLine(string);
+            gs = growStrCreate();
+        }
+        else
+        {
+            growStrAdd(gs, character);
+            //printf("Element %c added\n", character);
+            //printf("%s\n", gs->content);
+        }
+    }
     //  Convert Growstr into an array.
 
     //  Send the line into a processing fucntion. (Returns Subject)
     // parseLine();
 
-
-
-    if (fclose(file) != 0) {
+    if (fclose(file) != 0)
+    {
         printf("Sussy bakka cummy fucca!");
     }
 }
@@ -51,8 +81,10 @@ void parseFile(char* filePath, int *subjc, Subject *subjv) {
  * @param argv values of argumets.
  * @return Path to file from arg or NULL if no path was given
 */
-char* readFilePath(int argc, char **argv) {
-    if (argc < 1) {
+char *readFilePath(int argc, char **argv)
+{
+    if (argc < 1)
+    {
         return NULL;
     }
 
@@ -60,15 +92,23 @@ char* readFilePath(int argc, char **argv) {
     return argv[1];
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Getting the path to the file.
-    char* filePath = readFilePath(argc, argv);
-    if (filePath == NULL) {
+    char *filePath = readFilePath(argc, argv);
+    if (filePath == NULL)
+    {
         return 1;
     }
+
+    Subject subjekt;
+    int count = 5;
+    int *count_p;
+    count_p = &count;
+
+    parseFile(filePath, count_p, &subjekt);
 
     // Parsing the content into structs.
     //parseFile(filePath); // TODO Dunno co to bude vracet to vymyslíme později
     return 0;
 }
-
